7b8c85bad3fd738cc4d953c12d44990de8399786
Marta Maachowicz <malachowicz.marta@gmail.com>
2020-10-14 13:38:54 +0200

    added delete btn

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { getTodos, deleteTodo } from "./Requests";
+import { getTodos, deleteTodos } from "./Requests";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
@@ -7,19 +7,42 @@ function App() {
   const [todos, updateTodos] = useState([]);
 
   useEffect(() => {
+    getAndRenderTodos();
+  }, []);
+
+  const getAndRenderTodos = () => {
     getTodos().then((response) => {
       const { data } = response;
       updateTodos(data);
     });
-  }, []);
+  };
+
+  const onDelete = (id) => {
+    deleteTodos(id);
+    getAndRenderTodos();
+  };
 
   return (
     <main>
-      <ul>
+      <div>
+        <h1>Twoja lista zadań:</h1>
         {todos.map((todo) => {
-          return <li>{todo.title}</li>;
+          return (
+            <div className="todo" key={todo.id}>
+              <h2>{todo.title}</h2>
+              <p>{todo.description}</p>
+              <button
+                onClick={() => {
+                  onDelete(todo.id);
+                }}
+                className="btn btn-danger"
+              >
+                Usuń
+              </button>
+            </div>
+          );
         })}
-      </ul>
+      </div>
     </main>
   );
 }
